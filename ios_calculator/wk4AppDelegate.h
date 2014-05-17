22b166e038ffa8c248499da330dcb7f285817954
Dave Wieringa <dwjm@userwise.com>
2014-05-17 16:07:11 -0700

    Initial Commit

---
+++ ios_calculator/wk4AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  wk4AppDelegate.h
+//  ios_calculator
+//
+//  Created by David Wieringa on 5/17/14.
+//  Copyright (c) 2014 Userwise Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface wk4AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios_calculatorTests/ios_calculatorTests.m
@@ -0,0 +1,34 @@
+//
+//  ios_calculatorTests.m
+//  ios_calculatorTests
+//
+//  Created by David Wieringa on 5/17/14.
+//  Copyright (c) 2014 Userwise Solutions. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ios_calculatorTests : XCTestCase
+
+@end
+
+@implementation ios_calculatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
